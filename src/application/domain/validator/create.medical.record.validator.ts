054db9b2b91f72eeba2830d6e75004e4cffdcce2
Lucas Rocha <lucas.rocha@nutes.uepb.edu.br>
2019-03-21 21:23:02 -0300

    [FT05] - Cadastro de Paciente
    User Stories: Como profissional de saúde eu quero realizar o cadastro de um paciente por meio de um questionário.
    Bugs: Nenhum.
    Outras Informações: Adicionado validador para criação de MedicalRecord.

--- src/application/domain/validator/create.medical.record.validator.ts
+++ src/application/domain/validator/create.medical.record.validator.ts
@@ -1,13 +1,16 @@
 import { MedicalRecord } from '../model/medical.record'
 import { CreateActivityHabitsRecordValidator } from './create.activity.habits.record.validator'
 import { ValidationException } from '../exception/validation.exception'
+import { ChronicDiseaseValidator } from './chronic.disease.validator'
 
 export class CreateMedicalRecordValidator {
     public static validate(item: MedicalRecord): void | ValidationException {
-        const fields: Array<string> = []
+        let fields: Array<string> = []
         CreateActivityHabitsRecordValidator.validate(item)
         if (!item.chronic_diseases) fields.push('chronic_diseases')
 
+        const validChronicDisease = ChronicDiseaseValidator.validate(item.chronic_diseases)
+        fields = [...fields, ...validChronicDisease instanceof Array ? validChronicDisease : []]
 
         if (fields.length > 0) {
             throw new ValidationException('Required fields were not provided...',

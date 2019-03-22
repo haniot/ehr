d4e987359f00af580dce5b30a509bd690661fc66
Lucas Cosmo Rocha <lucas.rocha@nutes.uepb.edu.br>
2019-03-22 09:54:39 -0300

    [FT05] - Cadastro de Paciente
    User Stories: Como profissional de saúde eu quero realizar o cadastro de um paciente por meio de um questionário.
    Bugs: Nenhum.
    Outras Informações: Adicionado vexceção para atualizaÃção do parâmetro patient_id.

--- src/application/domain/validator/update.activity.habits.record.validator.ts
+++ src/application/domain/validator/update.activity.habits.record.validator.ts
@@ -1,10 +1,12 @@
-import { ObjectIdValidator } from './object.id.validator'
 import { ISODateTimeFormatValidator } from './iso.datetime.format.validator'
 import { ValidationException } from '../exception/validation.exception'
+import { Strings } from '../../../utils/strings'
 
 export class UpdateActivityHabitsRecordValidator {
     public static validate(item: any): void | ValidationException {
-        if (item.patient_id) ObjectIdValidator.validate(item.patient_id)
+        if (item.patient_id) {
+            throw new ValidationException('patient_id: '.concat(Strings.PARAMETERS.COULD_NOT_BE_UPDATED))
+        }
         if (item.created_at) ISODateTimeFormatValidator.validate(item.created_at)
     }
 }

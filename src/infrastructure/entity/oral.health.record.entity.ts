bf0f4acd4d895d5fdbdf9f82821abe76df2cc653
Italo Caio <it-caio@hotmail.com>
2019-07-12 12:50:49 -0300

    [FT07] - Refatorações Gerais
    User Stories: nenhum

--- src/infrastructure/entity/oral.health.record.entity.ts
+++
@@ -1,5 +0,0 @@
-export class OralHealthRecordEntity {
-    public teeth_brushing_freq?: string
-    public teeth_lesions?: Array<any>
-    public type?: string
-}
--- src/infrastructure/entity/sociodemographic.record.entity.ts
+++
@@ -1,6 +0,0 @@
-export class SociodemographicRecordEntity {
-    public color_race?: string
-    public mother_scholarity?: string
-    public people_in_home?: number
-    public type?: string
-}
